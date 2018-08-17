b7dc31e7de6d2d5515110e02f2fc82685bebef96
Oleh Melnyk <oleh.melnyk@gmail.com>
2018-08-17 23:31:12 +0300

    update task2 to calculate prize value on the fly and get rid of few functions

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -2,8 +2,7 @@ const gameInfoTemplate = (maxValue, attemptsLeft, totalPrize, currentPrize) => `
 Enter a number from 0 to ${maxValue}
 Attempts left: ${attemptsLeft}
 Total prize: ${totalPrize}$
-Possible prize on current attempt: ${currentPrize}$
-`;
+Possible prize on current attempt: ${currentPrize}$`;
 
 const config = {
     prizeMultiplier: 3,
@@ -15,11 +14,7 @@ const config = {
 Object.freeze(config);
 
 const gameData = {
-    prize: {
-        1: 10,
-        2: 5,
-        3: 2
-    },
+    prize: 10,
     maxRangeValue: 5,
     totalPrize: 0
 };
@@ -34,31 +29,37 @@ if(confirm('Do you want to play a game?')){
 
         const randomNumber = getRandomNumber(gameData.maxRangeValue);
 
-        for(let currentAttempt = 1; currentAttempt <= config.totalAttempts; currentAttempt++) {
+        for(
+            let currentAttempt = 1, currentPrize = gameData.prize;
+            currentAttempt <= config.totalAttempts;
+            currentAttempt++, currentPrize = Math.floor(currentPrize / 2)
+        ) {
             const userGuess = +prompt(
                 gameInfoTemplate(
                     gameData.maxRangeValue,
-                    ++config.totalAttempts - currentAttempt,
+                    config.totalAttempts - currentAttempt,
                     gameData.totalPrize,
-                    gameData.prize[currentAttempt]
+                    currentPrize
                 )
             );
 
             if (userGuess === randomNumber) {
-                updateTotalPrize(currentAttempt);
+                gameData.totalPrize += currentPrize;
                 if(confirm(`
-                    Congratulation! Your prize is: ${gameData.totalPrize} 
-                    Do you want to continue?`)) {
-                        updatePrizes();
-                        updateMaxRange();
-                        playAgain = true;
-                        break;
+                    Congratulation! Your prize is: ${gameData.totalPrize}$ 
+                    Do you want to continue?`)
+                ) {
+                    // update prize and maxRange values after correct guess
+                    gameData.prize *= config.prizeMultiplier;
+                    gameData.maxRangeValue *= config.rangeMultiplier;
+
+                    playAgain = true;
+                    break;
                 }
             } else if (currentAttempt === config.totalAttempts) {
                 alert(`Thank you for a game. Your prize is: ${gameData.totalPrize}$`);
             }
         }
-
     } while (playAgain);
 }
 
@@ -66,18 +67,4 @@ alert('You did not become a millionaire, but can.');
 
 function getRandomNumber(maxValue) {
     return Math.floor(Math.random() * ++maxValue);
-}
-
-function updateTotalPrize(currentAttempt) {
-    gameData.totalPrize += gameData.prize[currentAttempt];
-}
-
-function updatePrizes() {
-    gameData.prize['1'] *= config.prizeMultiplier;
-    gameData.prize['2'] *= config.prizeMultiplier;
-    gameData.prize['3'] *= config.prizeMultiplier;
-}
-
-function updateMaxRange() {
-    gameData.maxRangeValue *= config.rangeMultiplier;
 }
