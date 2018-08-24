784bee4ba52029407851c0185c3a8ef9c1fa5273
Oleh Melnyk <oleh.melnyk@gmail.com>
2018-08-24 13:51:32 +0300

    add 9th homework

---
+++ FE_9_9_homework_data-types/homework/index.js
@@ -0,0 +1,73 @@
+function findType(param) {
+    return typeof param;
+}
+
+function forEach(array, fn) {
+    for(let i = 0; i < array.length; i++) {
+         fn(array[i]);
+    }
+}
+
+function map(array, fn) {
+    let newArray = [];
+
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(fn(array[i]));
+    }
+
+    return newArray;
+}
+
+function filter(array, fn) {
+    let newArray = [];
+
+    for(let i = 0; i < array.length; i++) {
+        if(fn(array[i])){
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
+}
+
+function getAdultAppleLovers(data) {
+    let newArray = [];
+
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].age > 18 && data[i].favoriteFruit === 'apple') {
+            newArray.push(data[i].name);
+        }
+    }
+
+    return newArray;
+}
+
+function keys(obj) {
+    let newArray = [];
+
+    for(let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            newArray.push(key);
+        }
+    }
+
+    return newArray;
+}
+
+function values(obj) {
+    let newArray = [];
+
+    for(let key in obj) {
+        if(obj.hasOwnProperty(key)) {
+            newArray.push(obj[key]);
+        }
+    }
+
+    return newArray;
+}
+
+function showFormattedDate(date) {
+    const shortMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    return `It is ${date.getDate()} of ${shortMonthNames[date.getMonth()]}, ${date.getFullYear()}`;
+}
