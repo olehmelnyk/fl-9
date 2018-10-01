67a2fb4559fc61692b73c7c23140535084703c9a
Oleh Melnyk <oleh.melnyk@gmail.com>
2018-10-01 23:38:12 +0300

    add fix to remove product from old cart before adding to the new cart

--- offline/FL_9_3_homework_oop/homework/js/app.js
+++ offline/FL_9_3_homework_oop/homework/js/app.js
@@ -22,14 +22,14 @@ function isValidString(string) {
  * @returns {boolean}
  */
 function isValidPrice(price) {
-  return typeof price === 'number' && price >= 0 
-    && !Number.isNaN(price) && Number.isFinite(price);
+  return typeof price === 'number' && price >= 0
+      && !Number.isNaN(price) && Number.isFinite(price);
 }
 
 /**
  * Returns string with type of argument and it's string value in format:
  * "object '{a: 1, b: 2}'"
- * @param {*} param 
+ * @param {*} param
  * @returns {string}
  */
 function getArgumentInfo(param) {
@@ -45,6 +45,10 @@ function getArgumentInfo(param) {
  * @constructor
  */
 function Product(name, description, price) {
+
+  const _logs = [];
+  let _price, _productShoppingCart;
+
   if (isValidString(name)) {
     this.name = name.trim();
   } else {
@@ -64,7 +68,7 @@ ${getArgumentInfo(description)} received.`);
   }
 
   if (isValidPrice(price)) {
-    this.price = +price.toFixed(2);
+    _price = +price.toFixed(2);
   } else {
     console.error(`Wrong product price argument: expected positive number, \
 ${getArgumentInfo(price)} received.`);
@@ -72,15 +76,12 @@ ${getArgumentInfo(price)} received.`);
     return;
   }
 
-  const _logs = [];
-  let _cartName;
-
   /**
    * Returns the current product price
    * @returns {number} product price
    */
   this.getPrice = function() {
-    return this.price;
+    return _price;
   };
 
   /**
@@ -89,25 +90,33 @@ ${getArgumentInfo(price)} received.`);
    * @returns {Product}
    */
   this.setPrice = function(newPrice) {
-    if (isValidPrice(newPrice) && newPrice > this.price) {
-      _logs.push(`change price from ${this.price} to ${newPrice}`);
-      this.price = +newPrice.toFixed(2);
+    if (isValidPrice(newPrice) && newPrice > this.getPrice()) {
+      _logs.push(`change price from ${this.getPrice()} to ${newPrice}`);
+      _price = +newPrice.toFixed(2);
     } else {
-      _logs.push(`failed attempt to change price from ${this.price} to ${newPrice}`);
+      _logs.push(`failed attempt to change price from ${this.getPrice()} to ${newPrice}`);
     }
 
     return this;
   };
 
   /**
-   * Adds name of the shopping cart product placed in
+   * Returns shopping cart product placed in
+   * @return {(ShoppingCart|undefined)}
+   */
+  this.get = function() {
+    return _productShoppingCart;
+  };
+
+  /**
+   * Sets shopping cart product placed in
    * @param {ShoppingCart} shoppingCart instance of ShoppingCart
    * @return {Product}
    */
   this.add = function(shoppingCart) {
     if (shoppingCart instanceof ShoppingCart) {
-      _cartName = shoppingCart.name;
-      _logs.push(`${this.name} is added to ${_cartName} on ${getDate()}`);
+      _productShoppingCart = shoppingCart;
+      _logs.push(`${this.name} is added to ${_productShoppingCart.name} on ${getDate()}`);
     } else {
       console.error('add(shoppingCart) argument should be an instance of ShoppingCart');
     }
@@ -116,13 +125,13 @@ ${getArgumentInfo(price)} received.`);
   };
 
   /**
-   * Removes name of the shopping cart product placed in
+   * Removes shopping cart product placed in
    * @return {Product}
    */
   this.removeProduct = function(shoppingCart) {
     if (shoppingCart instanceof ShoppingCart) {
-      _logs.push(`${this.name} is removed from ${_cartName} on ${getDate()}`);
-      _cartName = null;
+      _logs.push(`${this.name} is removed from ${_productShoppingCart.name} on ${getDate()}`);
+      _productShoppingCart = null;
     } else {
       console.error('removeProduct(shoppingCart) argument should be an instance of ShoppingCart');
     }
@@ -185,6 +194,14 @@ ${getArgumentInfo(maxCount)} received.`);
    */
   this.addNewProduct = function(product) {
     if (product instanceof Product) {
+      if (product.get()) {
+        if (product.get().name !== this.name) {
+          product.get().removeProduct(product);
+        } else {
+          return this;
+        }
+      }
+
       if (_products.length >= this.maxCount) {
         this.removeCheapestProduct();
       }
@@ -231,11 +248,11 @@ ${getArgumentInfo(maxCount)} received.`);
    * Removes the cheapest product from the cart
    */
   this.removeCheapestProduct = function() {
-    let minIndex = 0, minPrice = _products[0].price;
+    let minIndex = 0, minPrice = _products[0].getPrice();
 
     for (let i = 1; i < _products.length; i++) {
-      if (_products[i].price < minPrice) {
-        minPrice = _products[i].price;
+      if (_products[i].getPrice() < minPrice) {
+        minPrice = _products[i].getPrice();
         minIndex = i;
       }
     }
@@ -278,7 +295,7 @@ Detailed product description: ${JSON.stringify(product.description)}`);
    * @return {number} total price
    */
   this.getTotalPrice = function() {
-    return +_products.reduce((accumulator, current) => accumulator + current.price, 0).toFixed(2);
+    return +_products.reduce((accumulator, current) => accumulator + current.getPrice(), 0).toFixed(2);
   };
 
   /**
@@ -291,39 +308,56 @@ Detailed product description: ${JSON.stringify(product.description)}`);
 }
 
 /********************
-*     Code demo
-********************/
-const banana = new Product('banana', {color: 'yellow', size: 'medium'}, 45);
+ *     Code demo
+ ********************/
+const banana1 = new Product('banana1', {color: 'yellow', size: 'medium'}, 45);
+const banana2 = new Product('banana2', {color: 'yellow', size: 'medium'}, 45);
+const banana3 = new Product('banana3', {color: 'yellow', size: 'medium'}, 45);
+const banana4 = new Product('banana4', {color: 'yellow', size: 'medium'}, 45);
+const banana5 = new Product('banana5', {color: 'yellow', size: 'medium'}, 45);
 const apple = new Product('apple', {color: 'red', size: 'small'}, 30);
 
 const stevesShopCart = new ShoppingCart('stevesCart', 'Steve', 5);
 
 stevesShopCart
-  .addNewProduct(banana)
-  .addNewProduct(banana)
+  .addNewProduct(banana1)
+  .addNewProduct(banana2)
   .addNewProduct(apple)
-  .removeProduct(banana);
+  .removeProduct(banana1);
 
 console.log(
     'Cart history:', stevesShopCart.getHistory(),
-    'Product history:', banana.getHistory());
+    'Product history:', banana2.getHistory());
 
 console.log('average price:', stevesShopCart.getAveragePrice());
 console.log('total price:', stevesShopCart.getTotalPrice());
 
-console.log('banana price:', banana.getPrice());
-banana.setPrice(20).setPrice(100);
+console.log('banana price:', banana2.getPrice());
+banana2.setPrice(20).setPrice(100);
 
 // let's make sure the cheaper apple is removed to free-up space for more bananas
 stevesShopCart
-  .addNewProduct(banana)
-  .addNewProduct(banana)
-  .addNewProduct(banana)
-  .addNewProduct(banana);
+  .addNewProduct(banana3)
+  .addNewProduct(banana4)
+  .addNewProduct(banana5)
+  .addNewProduct(banana1);
 
-console.log('Product history:', banana.getHistory());
+console.log('Product history:', banana1.getHistory());
 
 console.log(`${stevesShopCart.owner} products:`);
 console.table(stevesShopCart.getFormattedListOfProducts());
 
 stevesShopCart.addNewProduct('apple string'); // should show an error msg
+
+// now let's try to create a new ShoppingCart and put there product from the Steve's cart
+const olehShoppingCart = new ShoppingCart('Oleh Shopping Cart', 'Oleh', 3);
+
+olehShoppingCart.addNewProduct(banana1); // put banana1 from steveCart to olehCart
+olehShoppingCart.addNewProduct(banana1); // we don't add same banana twice to the same cart
+
+console.log(`${olehShoppingCart.owner} products:`);
+console.table(olehShoppingCart.getFormattedListOfProducts());
+
+// let's check stevesShopCart for the last time
+console.log(`${stevesShopCart.owner} products:`);
+console.table(stevesShopCart.getFormattedListOfProducts());
